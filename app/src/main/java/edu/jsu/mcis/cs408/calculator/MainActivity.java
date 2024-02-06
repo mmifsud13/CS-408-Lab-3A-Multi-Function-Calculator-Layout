7e5145bd296f98ec1c03d4965a0c844ea1de6b55
Matthew Mifsud <mmifsud1999@gmail.com>
2024-02-06 13:59:58 -0600

    Initial Commit

---
+++ app/src/main/java/edu/jsu/mcis/cs408/calculator/MainActivity.java
@@ -0,0 +1,21 @@
+package edu.jsu.mcis.cs408.calculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+import edu.jsu.mcis.cs408.calculator.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+    }
+}
